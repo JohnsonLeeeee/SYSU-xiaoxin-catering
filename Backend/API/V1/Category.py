6c89718d63fa5d555006508d941d2055cda883f0
yyhyplxyz <yangyh57@mail2.sysu.edu.cn>
2019-06-22 11:07:28 +0800

    fix bugs

--- Backend/API/V1/Category.py
+++ Backend/API/V1/Category.py
@@ -4,7 +4,7 @@ from Backend.libs.exception_api import  Success
 from Backend.libs.MyBluePrint import MyBluePrint
 from Backend.libs.auth import auth
 from Backend.Model.FoodCategory import FoodCat
-from Backend.Viewmodel.Comment import CommentViewModel
+from Backend.Viewmodel.Category import CategoryViewModel
 
 api = MyBluePrint('category')
 
@@ -12,6 +12,6 @@ api = MyBluePrint('category')
 @auth.login_required
 def get_restaurant_category(rid):
     category = FoodCat.query.filter_by(rid=rid).all()
-    list = [CommentViewModel.comment(i) for i in category]
+    list = [CategoryViewModel(i) for i in category]
     return jsonify(category = list, number = len(list))
 
