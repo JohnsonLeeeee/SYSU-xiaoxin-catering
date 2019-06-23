a77e3b0b8a1713ec8b30d85433e1fb4d112c17e0
JohnsonLeeeee <lishlin3@mail2.sysu.edu.cn>
2019-06-23 11:30:25 +0800

    增添了授权界面

--- Frontend/Xiaoxin-catering/pages/index/index.js
+++ Frontend/Xiaoxin-catering/pages/index/index.js
@@ -1,72 +1,87 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
   data: {
-    motto: '欢迎来到小欣厨房',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad: function () {
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              //从数据库获取用户信息
+              that.queryUsreInfo();
+              //用户已经授权过
+              
+              wx.switchTab({
+                url: 'pages/auth/auth'
+              })
+            }
+          });
+        }
+      }
     })
   },
-  
-  bindGetUserInfo(res) {
-    console.log(res);
-    if (res.detail.userInfo) {
-      console.log("点击了同意授权");
-    } else {
-      console.log("点击了拒绝授权");
-    }
-  },
-
-  onLoad: function () {
-    setTimeout(function () {
-      wx.redirectTo({
-        url: '../menu/menu',
+  bindGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      //插入登录的用户的相关信息到数据库
+      wx.request({
+        url: app.globalData.urlPath + 'user/add',
+        data: {
+          openid: getApp().globalData.openid,
+          nickName: e.detail.userInfo.nickName,
+          avatarUrl: e.detail.userInfo.avatarUrl,
+          province: e.detail.userInfo.province,
+          city: e.detail.userInfo.city
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          //从数据库获取用户信息
+          that.queryUsreInfo();
+          console.log("插入小程序登录用户信息成功！");
+        }
+      });
+      //授权成功后，跳转进入小程序首页
+      wx.switchTab({
+        url: 'pages/auth/auth'
       })
-    }, 3000);
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”')
+          }
         }
       })
     }
   },
-
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  //获取用户信息接口
+  queryUsreInfo: function () {
+    wx.request({
+      url: app.globalData.urlPath + 'user/userInfo',
+      data: {
+        openid: app.globalData.openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        getApp().globalData.userInfo = res.data;
+      }
+    }) 
   },
+
 })
