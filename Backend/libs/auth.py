48d6cbbbd8e484bdce4b3f8e0e121b814199761e
yyhyplxyz <yangyh57@mail2.sysu.edu.cn>
2019-06-24 16:28:16 +0800

    修改

--- Backend/libs/auth.py
+++ Backend/libs/auth.py
@@ -1,13 +1,8 @@
 
 from collections import namedtuple
 
-from flask import current_app, g, request
+from flask import g
 from flask_httpauth import HTTPBasicAuth
-from itsdangerous import TimedJSONWebSignatureSerializer \
-    as Serializer, BadSignature, SignatureExpired
-
-from.exception_api import AuthFailed, Forbidden
-from .scope import is_in_scope
 
 
 auth = HTTPBasicAuth()
@@ -43,23 +38,3 @@ def verify_password(token, password):
         # request
         g.user = user_info
         return True
-
-
-def verify_auth_token(token):
-    s = Serializer(current_app.config['SECRET_KEY'])
-    try:
-        data = s.loads(token)
-    except BadSignature:
-        raise AuthFailed(msg='token is invalid',
-                         error_code=1002)
-    except SignatureExpired:
-        raise AuthFailed(msg='token is expired',
-                         error_code=1003)
-    uid = data['uid']
-    ac_type = data['type']
-    scope = data['scope']
-    # request 视图函数
-    allow = is_in_scope(scope, request.endpoint)
-    if not allow:
-        raise Forbidden()
-    return User(uid, ac_type, scope)
