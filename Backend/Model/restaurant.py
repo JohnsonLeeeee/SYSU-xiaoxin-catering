42ee1764a1776dc15bbe386664d8232e0ec72d1e
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2019-06-27 16:25:33 +0800

    modify Model3

--- Backend/Model/restaurant.py
+++ Backend/Model/restaurant.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, FetchedValue, String, DateTime, Numeric, ForeignKey
-from sqlalchemy.orm import relationship
+# -*- coding:utf-8 -*-
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import FetchedValue
+from sqlalchemy import String
+
 from .base import Base
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False, server_default=FetchedValue())
     main_image = Column(String(100), server_default=FetchedValue())
     summary = Column(String(2000), server_default=FetchedValue())
