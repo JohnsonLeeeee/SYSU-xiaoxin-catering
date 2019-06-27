90be1222c9ba23a2b346eede908cd46310d748d0
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2019-06-27 16:25:07 +0800

    modify Model

--- Backend/Model/Comment.py
+++ Backend/Model/Comment.py
@@ -1,13 +1,18 @@
-from sqlalchemy import Integer, Column, String, FetchedValue,  ForeignKey, DateTime,func
+# -*- coding:utf-8 -*-
+from sqlalchemy import Integer
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import FetchedValue
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from .base import Base, db
 
+from .base import Base
 
 
 class Comment(Base):
     __tablename__ = 'Comment'
 
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     uid = Column(Integer, ForeignKey('user.id'), nullable=False, server_default=FetchedValue())
     user = relationship('User')
     rid = Column(Integer, ForeignKey('restaurant.id'), nullable=False, server_default=FetchedValue())
